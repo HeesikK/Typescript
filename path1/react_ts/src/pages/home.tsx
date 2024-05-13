6a61b9a16d88f751ecf76ddac68ecd6a3e9f30f2
HeesikK <7jjhkhz@naver.com>
2024-05-13 19:15:07 +0900

    feat: api 데이터 출력(map)

--- path1/react_ts/src/pages/home.tsx
+++ path1/react_ts/src/pages/home.tsx
@@ -6,10 +6,28 @@ import { getMovieList } from "../apis/api";
 const HomePage = () => {
   const { data: movieList } = useQuery("movieList", () => getMovieList());
 
-  console.log(movieList);
+  interface Movie {
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+  }
+
+  const movieData: Movie[] = movieList && movieList.results;
+  console.log("영화 데이터값은?", movieData);
 
   return (
     <div>
+      {movieData.map((data) => (
+        <div>{data.title}</div>
+      ))}
       <CustomButton variant="primary" size="small" shape="round">
         button
       </CustomButton>
