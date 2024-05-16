fb59f1874b64f8bf92bdd5ae8326c6630d8c91f1
HeesikK <7jjhkhz@naver.com>
2024-05-16 14:10:10 +0900

    feat: 로그인/회원가입 폼 퍼블리싱 완료

--- path1/auth_ts/src/page/sign-in/index.tsx
+++ path1/auth_ts/src/page/sign-in/index.tsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+import HSButton from "../../component/button";
+import HSInput from "../../component/input";
+
 const SignInPage = () => {
-  return <>로그인</>;
+  return (
+    <Container>
+      <HSInput variant="primary" size="large" shape="shape" type="email" placeholder="이메일을 입력해주세요" children="이메일" />
+      <HSInput variant="primary" size="large" shape="shape" type="password" placeholder="비밀번호를 입력해주세요" children="비밀번호" />
+      <HSButton variant="primary" size="large" shape="shape" children="로그인" />
+    </Container>
+  );
 };
 
 export default SignInPage;
+
+const Container = styled.div`
+  & > * {
+    padding-bottom: 20px;
+  }
+`;
--- path1/auth_ts/src/page/sign-up/index.tsx
+++ path1/auth_ts/src/page/sign-up/index.tsx
@@ -7,9 +7,7 @@ const SignUpPage = () => {
     <Container>
       <HSInput variant="primary" size="large" shape="shape" type="email" placeholder="이메일을 입력해주세요" children="이메일" />
       <HSInput variant="primary" size="large" shape="shape" type="password" placeholder="비밀번호를 입력해주세요" children="비밀번호" />
-      <HSButton variant="primary" size="large" shape="shape">
-        회원가입
-      </HSButton>
+      <HSButton variant="primary" size="large" shape="shape" children="회원가입" />
     </Container>
   );
 };
