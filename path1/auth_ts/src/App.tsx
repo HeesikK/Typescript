53dac682997c7f5f23607dd426fda4f70cd8e61e
HeesikK <7jjhkhz@naver.com>
2024-05-16 13:33:15 +0900

    feat: styled-component 테마 적용

--- path1/auth_ts/src/App.tsx
+++ path1/auth_ts/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
+import { ThemeProvider } from "styled-components";
+import theme from "./style/theme.style";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   );
 }
 
