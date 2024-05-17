e3c4ce057e8a7b23baf5f5e692530d063fd951cf
HeesikK <7jjhkhz@naver.com>
2024-05-17 12:54:27 +0900

    feat: regex 수정 및 모듈화

--- path1/auth_ts/src/type/sign.type.ts
+++ path1/auth_ts/src/type/sign.type.ts
@@ -7,5 +7,3 @@ export type SignUpType = {
 //   email: string;
 //   password: string;
 // };
-
-export type SignInField = "email" | "password";
--- path1/auth_ts/src/utils/validate.tsx
+++ path1/auth_ts/src/utils/validate.tsx
@@ -1,17 +1,19 @@
 // utils/validate.jsx
 
+import { regex } from "../const/regex";
+
 export const validate = {
   email: {
     required: "이메일은 필수입니다.",
     pattern: {
-      value: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+      value: regex.email,
       message: "이메일 형식에 맞지 않습니다.",
     },
   },
   password: {
     required: "비밀번호는 필수입니다.",
     pattern: {
-      value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&^#~])[A-Za-z\d@$!%*?&^#~]{8,}$/,
+      value: regex.password,
       message: "비밀번호는 대소문자, 특수문자를 포함하여 8자 이상이어야 합니다.",
     },
     minLength: {
